efda37f6d279c48bd0ce0e6b71abfce5eb8b0fe8
Dorian Beganovic <beganovic.dorian@gmail.com>
2024-03-03 10:48:14 +0000

    cleanup

--- property_cro/macros/inventory_macro.sql
+++ property_cro/macros/inventory_macro.sql
@@ -1,14 +1,12 @@
 {% macro inventory_macro(mat_view, source_schema, source_table, price_history_view) %}
 
-{% if "duckdb" in target.name %}
-
 with zg_loc as (
     select distinct location from {{ ref(mat_view) }}
     where location2 like 'Grad Zagreb,%' or location2 like 'Zagrebačka,%'
 ),
 new_ads as (
     select
-        cast(strftime(first_seen,'%Y%m') as integer) as year_month,
+        date_trunc('month', first_seen) as year_month,
         count(distinct ad_id) filter (where price_m2 < 10000) as new_ads_qty,
         round((avg(days_on_the_market) filter (where price_m2 < 10000))::numeric,1) as new_ads_dom,
         round((avg(price_m2) filter (where price_m2 < 10000))::numeric,1) as new_ads_avg_px_m2,
@@ -16,10 +14,10 @@ new_ads as (
         round((avg(size) filter (where price_m2 < 10000))::numeric,1) as new_ads_avg_size
     from {{ ref(mat_view) }}
     where location in (select location from zg_loc)
-    group by cast(strftime(first_seen, '%Y%m') as integer)
+    group by date_trunc('month', first_seen)
 ), sold_ads as (
     select
-        cast(strftime(last_seen,'%Y%m') as integer) as year_month,
+        date_trunc('month', last_seen) as year_month,
         count(distinct ad_id) filter (where price_m2 < 10000) as sold_ads_qty,
         round((avg(days_on_the_market) filter (where price_m2 < 10000))::numeric,1) as sold_ads_dom,
         round((avg(price_m2) filter (where price_m2 < 10000))::numeric,1) as sold_ads_avg_px_m2,
@@ -27,10 +25,10 @@ new_ads as (
         round((avg(size) filter (where price_m2 < 10000))::numeric,1) as sold_ads_avg_size
     from {{ ref(mat_view) }}
     where status = 'inactive' and location in (select location from zg_loc)
-    group by cast(strftime(last_seen,'%Y%m') as integer)
+    group by date_trunc('month', last_seen)
 ), actives as (
     select
-        cast(strftime(seen_date,'%Y%m') as integer) as year_month,
+        date_trunc('month', seen_date) as year_month,
         count(distinct s.ad_id) filter (where s.price < 10000000) as active_ads_qty,
         round((avg(days_on_the_market) filter (where s.price < 10000000))::numeric,1) as active_ads_dom,
         round((avg(s.price/s.size) filter (where s.price < 10000000))::numeric,1) as active_ads_avg_px_m2,
@@ -40,10 +38,10 @@ new_ads as (
     join {{ ref(price_history_view) }} as hist2
     on s.ad_id = hist2.ad_id
     where s.location in (select location from zg_loc)
-    group by cast(strftime(seen_date, '%Y%m') as integer)
+    group by date_trunc('month', seen_date)
 )
 select
-    strptime(concat(n.year_month,'01'), '%Y%m%d') as year_month,
+    n.year_month,
     new_ads_qty,
     new_ads_dom,
     new_ads_avg_px_m2,
@@ -63,74 +61,5 @@ from new_ads n
          join sold_ads r on n.year_month = r.year_month
          join actives a on n.year_month = a.year_month
 order by n.year_month desc
-
-{% elif "postgres" in target.name %}
-
-with zg_loc as (
-    select distinct location from {{ ref(mat_view) }}
-    where location2 like 'Grad Zagreb,%'
-),
-new_ads as (
-    select
-        cast(to_char(first_seen,'YYYYmm') as integer) as year_month,
-        count(distinct ad_id) filter (where price_m2 < 10000) as new_ads_qty,
-        round((avg(days_on_the_market) filter (where price_m2 < 10000))::numeric,1) as new_ads_dom,
-        round((avg(price_m2) filter (where price_m2 < 10000))::numeric,1) as new_ads_avg_px_m2,
-        round((avg(price) filter (where price_m2 < 10000))::numeric,1) as new_ads_avg_px,
-        round((avg(size) filter (where price_m2 < 10000))::numeric,1) as new_ads_avg_size
-    from {{ ref(mat_view) }}
-    where location in (select location from zg_loc)
-    group by cast(to_char(first_seen, 'YYYYmm') as integer)
-), sold_ads as (
-    select
-        cast(to_char(last_seen,'YYYYmm') as integer) as year_month,
-        count(distinct ad_id) filter (where price_m2 < 10000) as sold_ads_qty,
-        round((avg(days_on_the_market) filter (where price_m2 < 10000))::numeric,1) as sold_ads_dom,
-        round((avg(price_m2) filter (where price_m2 < 10000))::numeric,1) as sold_ads_avg_px_m2,
-        round((avg(price) filter (where price_m2 < 10000))::numeric,1) as sold_ads_avg_px,
-        round((avg(size) filter (where price_m2 < 10000))::numeric,1) as sold_ads_avg_size
-    from {{ ref(mat_view) }}
-    where status = 'inactive'
-      and location in (select location from zg_loc)
-    group by cast(to_char(last_seen,'YYYYmm') as integer)
-), actives as (
-    select
-        cast(to_char(seen_date,'YYYYmm') as integer) as year_month,
-        count(distinct s.ad_id) filter (where s.price < 10000000) as active_ads_qty,
-        round((avg(days_on_the_market) filter (where s.price < 10000000))::numeric,1) as active_ads_dom,
-        round((avg(s.price/s.size) filter (where s.price < 10000000))::numeric,1) as active_ads_avg_px_m2,
-        round((avg(s.price) filter (where s.price < 10000000))::numeric,1) as active_ads_avg_px,
-        round((avg(s.size) filter (where s.price < 10000000))::numeric,1) as active_ads_avg_size
-    from {{ source(source_schema, source_table) }} s
-    join {{ ref(price_history_view) }} as hist2
-    on s.ad_id = hist2.ad_id
-    where s.location in (select location from zg_loc)
-    group by cast(to_char(seen_date, 'YYYYmm') as integer)
-)
-select
-    TO_DATE(concat(n.year_month,'01'), 'YYYYMMDD') as year_month,
-    new_ads_qty,
-    new_ads_dom,
-    new_ads_avg_px_m2,
-    new_ads_avg_px,
-    new_ads_avg_size,
-    sold_ads_qty,
-    sold_ads_dom,
-    sold_ads_avg_px_m2,
-    sold_ads_avg_px,
-    sold_ads_avg_size,
-    active_ads_qty,
-    active_ads_dom,
-    active_ads_avg_px_m2,
-    active_ads_avg_px,
-    active_ads_avg_size
-from new_ads n
-         join sold_ads r on n.year_month = r.year_month
-         join actives a on n.year_month = a.year_month
-order by n.year_month desc
-
-{%- else -%} invalid_database
-
-{% endif %}
 
 {% endmacro %}
